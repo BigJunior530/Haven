933773ac537e63a5e3d37e69ad8f88b6e6a132b0
JaimeR530 <43947537+BigJunior530@users.noreply.github.com>
2019-11-28 22:33:22 -0800

    Create WildBoar.java
    Needs additional getters and setters.

---
+++ WildBoar.java
@@ -0,0 +1,26 @@
+public class WildBoar extends CharEntities{
+      private int health;
+      private int attack;
+
+      public WildBoar(){
+            this(5,3);
+      }
+      public WildBoar(int health, int attack){
+            this.health = health;
+            this.attack = attack;
+      }
+
+      public int getAttack(){
+            return this.attack;
+      }
+
+      public void intro() throws InterruptedException {
+            Main.sleep500();
+            Thread.sleep(1000);
+            System.out.println("\n\nA wild boar appeared!");
+      }
+
+      public int getHealth(){
+            return this.health;
+      }
+}
