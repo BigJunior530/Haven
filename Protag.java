d42f280f2629409bdd25fb3fced6a2d70e142c5c
MasterBlaz3 <48111685+MasterBlaz3@users.noreply.github.com>
2019-11-28 23:59:54 -0800

    Update Protag.java

--- Protag.java
+++ Protag.java
@@ -1,30 +1,63 @@
-public class Protag extends CharEntities{
+public class Protag {
       private int health;
       private int attack;
       private int shield;
-      private int damageTaken;
+      private int exp;
+      private int level;
+      private int needed;
+
 
       public Protag(){
-            this (25, 8, 5, 0);
-      }
+            this.health = 100;
+            this.attack = 5;
+            this.shield = 2;
+            level = 1;
+            exp = 0;
+            needed = 100;
 
-      public Protag(int health, int attack, int shield, int damageTaken){
-            this.health = health;
-            this.attack = attack;
-            this.shield = shield;
-
-            if(damageTaken != 0){
-                  
-            }
       }
       
-      
+      public void setExp(int strength) {
+    	  exp = exp + (strength*2);
+      }
       public int getShield(){
-            return 0;
+            return shield;
+      }
+      public int getAttack(){
+          return attack;
+      }
+      public int getHealth(){
+          return health;
+      }
+      public int getExp() {
+    	  return exp;
+      }
+      public int getLevel() {
+    	  return level;
+      }
+      public int getNeeded() {
+    	  return needed;
+      }
+      public void setNeeded() {
+    	  needed = 10*getLevel();
+      }
+      
+      public void levelUp() {
+    	  health = health + 5;
+    	  attack = attack + 5;
+    	  shield = shield + 3;
+    	  exp = exp - getNeeded();
+    	  level++;
+    	  setNeeded();
+    	  
       }
 
-      public static void getItems(){
-            System.out.println();
+      public void damage(int hurt) {
+    	  health = health - hurt;
+      }
+
+      public void getItems(){
+            System.out.println("No Items");
       }
 
       
