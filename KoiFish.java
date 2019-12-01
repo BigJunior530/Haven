45876fc58b629e3ffad2117cf770ef2b44f11837
MasterBlaz3 <48111685+MasterBlaz3@users.noreply.github.com>
2019-12-01 14:58:19 -0800

    Update KoiFish.java

--- KoiFish.java
+++ KoiFish.java
@@ -36,6 +36,8 @@ public class KoiFish extends CharEntities{
           Main.sleep500();
           Thread.sleep(1000);
           System.out.println("\n\nA Koi Fish appeared!");
+          System.out.println("Level: " + getLevel());
+          System.out.println("Health: " + getHealth());
     }
 
     public int getHealth(){
