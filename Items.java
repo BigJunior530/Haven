ee1595a2b9d6c3efcd3b82aaa79daefcc4aac684
Carlos Caceres <48111685+MasterBlaz3@users.noreply.github.com>
2020-03-08 00:54:29 -0800

    Bountiful Update
    This update adds new items and equipment. Items now have three tiers, unlocking the higher tiers when you level up. Equipment now have two tiers, the high tier of each item will be unlocked as soon as you receive the first tier. Added the ability to confirm items and return to menu if you decide not to use. The item you first get will now be shower in the beginning of the story. Updated Ascii Art for Leviathan.

--- Items.java
+++ Items.java
@@ -5,15 +5,59 @@
  */
 public class Items {
 	/**
+	 * This method prints out the information for a boat
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static void boat() throws InterruptedException{
+		System.out.println("Boat");
+        Thread.sleep(1000);
+		System.out.println("Allows Access to Ocean region.");
+        Thread.sleep(1000);
+	}
+	/**
 	 * This method prints out the information for the Health Potion
 	 * 
 	 * @throws InterruptedException
 	 */
 	public static void healthPotion() throws InterruptedException{
 		System.out.println("Health Potion.");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 		System.out.println("Adds 25 health.");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
+	}
+	/**
+	 * This method prints out the information for the Booster Potion
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static void boosterPotion() throws InterruptedException{
+		System.out.println("Health Potion.");
+        Thread.sleep(1000);
+		System.out.println("Adds 50 health.");
+        Thread.sleep(1000);
+	}
+	/**
+	 * This method prints out the information for Adrenaline
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static void adrenaline() throws InterruptedException{
+		System.out.println("Adrenaline.");
+        Thread.sleep(1000);
+		System.out.println("Adds 100 health.");
+        Thread.sleep(1000);
+	}
+	/**
+	 * This method prints out the information for the Attack Potion
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static void attackPotion() throws InterruptedException{
+		System.out.println("Attack Potion.");
+        Thread.sleep(1000);
+		System.out.println("Adds 3 attack.");
+        Thread.sleep(1000);
 	}
 	/**
 	 * This method prints out the information for the Strength Potion
@@ -22,9 +66,20 @@ public class Items {
 	 */
 	public static void strengthPotion() throws InterruptedException{
 		System.out.println("Strength Potion.");
-        Thread.sleep(2000);
-		System.out.println("Adds 3 attack.");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
+		System.out.println("Adds 6 attack.");
+        Thread.sleep(1000);
+	}
+	/**
+	 * This method prints out the information for the Steriods
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static void steroids() throws InterruptedException{
+		System.out.println("Steroids.");
+        Thread.sleep(1000);
+		System.out.println("Adds 10 attack.");
+        Thread.sleep(1000);
 	}
 	/**
 	 * This method prints out the information for the Luck Potion
@@ -33,9 +88,42 @@ public class Items {
 	 */
 	public static void LuckPotion() throws InterruptedException{
 		System.out.println("Luck Potion.");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 		System.out.println("Adds 1 Luck.");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
+	}
+	/**
+	 * This method prints out the information for the Fortunre Potion
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static void fortunePotion() throws InterruptedException{
+		System.out.println("Fortune Potion.");
+        Thread.sleep(1000);
+		System.out.println("Adds 2 Luck.");
+        Thread.sleep(1000);
+	}
+	/**
+	 * This method prints out the information for a Four Leaf Clover
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static void clover() throws InterruptedException{
+		System.out.println("Four Leaf Clover.");
+        Thread.sleep(1000);
+		System.out.println("Adds 4 Luck.");
+        Thread.sleep(1000);
+	}
+	/**
+	 * This method prints out the information for Scotch tape
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static void scotchTape() throws InterruptedException{
+		System.out.println("Scotch Tape.");
+        Thread.sleep(1000);
+		System.out.println("Adds 2 shield.");
+        Thread.sleep(1000);
 	}
 	/**
 	 * This method prints out the information for duct tape
@@ -44,8 +132,19 @@ public class Items {
 	 */
 	public static void ductTape() throws InterruptedException{
 		System.out.println("Duct Tape.");
-        Thread.sleep(2000);
-		System.out.println("Adds 2 shield.");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
+		System.out.println("Adds 4 shield.");
+        Thread.sleep(1000);
+	}
+	/**
+	 * This method prints out the information for Flex tape
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static void flexTape() throws InterruptedException{
+		System.out.println("Flex Tape.");
+        Thread.sleep(1000);
+		System.out.println("Adds 6 shield.");
+        Thread.sleep(1000);
 	}
 }
